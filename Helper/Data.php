b70e53cc18efe3249330997bca67bfe015821700
Muzaffer Kılıçaslan <muzaffer@magentoteknik.com>
2024-12-16 23:46:59 +0300

    feat: created helper file

---
+++ Helper/Data.php
@@ -0,0 +1,60 @@
+<?php
+/**
+ * Copyright ©  All rights reserved.
+ * See COPYING.txt for license details.
+ */
+declare(strict_types=1);
+
+namespace Shipment\CustomShipment\Helper;
+
+use Magento\Framework\App\Helper\AbstractHelper;
+use Magento\Store\Model\ScopeInterface;
+
+class Data extends AbstractHelper
+{
+
+    /**
+     * @return bool
+     */
+    public function isActive()
+    {
+        return $this->scopeConfig->isSetFlag(
+            'carriers/express_shipping/is_active',
+            ScopeInterface::SCOPE_STORE,
+        );
+    }
+
+    /**
+     * @return string
+     */
+    public function weightLimitForMinPrice()
+    {
+        return $this->scopeConfig->getValue(
+            'carriers/express_shipping/weight_limit_for_min_price',
+            ScopeInterface::SCOPE_STORE,
+        );
+    }
+
+    /**
+     * @return string
+     */
+    public function additionalPrice()
+    {
+        return $this->scopeConfig->getValue(
+            'carriers/express_shipping/additional_price',
+            ScopeInterface::SCOPE_STORE,
+        );
+    }
+
+    /**
+     * @return string
+     */
+    public function maximumWeightLimit()
+    {
+        return $this->scopeConfig->getValue(
+            'carriers/express_shipping/maximum_weight_limit',
+            ScopeInterface::SCOPE_STORE,
+        );
+    }
+
+}
